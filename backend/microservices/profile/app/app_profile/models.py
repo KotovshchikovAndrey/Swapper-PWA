7c4108184c7124d911045e6855673ad554aa872a
Andrey <ykt_andrey@mail.ru>
2023-01-28 23:39:10 +0300

    create profile models

--- backend/microservices/profile/app/app_profile/models.py
+++ backend/microservices/profile/app/app_profile/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100, null=False, verbose_name="Имя")
+    surname = models.CharField(max_length=100, null=False, verbose_name="Фамилия")
+    patronymic = models.CharField(max_length=100, null=True, verbose_name="Отчество")
+    email = models.EmailField(unique=True, null=False, verbose_name="Email")
+    age = models.IntegerField(null=False, verbose_name="Возраст")
+    phone = models.CharField(max_length=18, null=True, verbose_name="Телефон")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self) -> str:
+        return f"{self.surname} {self.name}"
+
+
+class Swap(models.Model):
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    date = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата совершения обмена"
+    )
+    description = models.TextField(null=False, verbose_name="Описание/Условия обмена")
+
+    class Meta:
+        verbose_name = "Обмен"
+        verbose_name_plural = "Обмены"
+
+    def __str__(self) -> str:
+        return f"Обмен с пользователем {self.user}"
+
+
+class SwapHistory(models.Model):
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    swaps = models.ManyToManyField("Swap", verbose_name="Обмены")
+
+    class Meta:
+        verbose_name_plural = "История обменов"
+
+    def __str__(self) -> str:
+        return f"История обменов пользователя {self.user}"
